d604fb190cd6b2b2f147b8d5c667f0dc3a858a49
Ibarra11 <alanjibarradev@gmail.com>
2023-11-20 14:47:57 -0800

    fix layout on desktop

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -16,18 +16,17 @@ export default function Question({
 }: React.PropsWithChildren<QuestionProps>) {
   const progress = Math.round((questionNumber / numberOfQuestions) * 100);
   return (
-    <div className="flex flex-col gap-10 md:gap-14 lg:flex-row lg:justify-between">
-      <div className="flex flex-col gap-6 md:gap-10 lg:h-[452px]">
+    <div className="flex flex-col gap-10 md:gap-14 lg:flex-row lg:gap-20">
+      <div className="flex flex-col gap-6 md:gap-10 lg:h-[452px] lg:w-[452px]">
         <div className="space-y-3 md:space-y-7 lg:mb-auto">
           <h3 className="text-sm text-light-blue italic md:text-xl">
             Question {questionNumber} out of {numberOfQuestions}
           </h3>
           <h2 className="text-xl text-white md:text-4xl">{question}</h2>
         </div>
-
         <CompletionBar progress={progress} />
       </div>
-      {children}
+      <div className="flex-1">{children}</div>
     </div>
   );
 }
