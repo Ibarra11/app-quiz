bcc47f2a9efa47f62a31b4abb4233f5ac4d70481
Ibarra11 <alanjibarradev@gmail.com>
2024-01-02 22:55:18 -0800

    add cors to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
+import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
 import * as db from "./db/index.js";
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/quiz", async (req, res) => {
